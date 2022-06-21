7c7c723027a8adb110623320c027107ff9e56102
Alex Acebo <alex.acebo@kustomer.com>
2022-06-21 11:57:36 -0400

    push changes

--- index.js
+++ index.js
@@ -6,19 +6,22 @@ const pkg = require('./package.json');
     app: pkg.name,
     version: pkg.version,
     title: 'Delighted (SDK)',
-    iconUrl: 'https://cdnapps.helpapp.io/delighted/images/icon.png',
-    description: `
-      Automatically retrieve survey results from Delighted and display NPS data in the timeline. \n
-      Use the Delighted app integration to allow agents to see a customer's NPS survey results from within the Kustomer timeline. The Delighted integration automatically creates and updates customer records whenever your customers submit an NPS survey through Delighted. This allows your team to have access to critical voice-of-customer data that can help set the tone for support interactions.
-      **Delighted** is a customer feedback platform that provides a seamless way to collect and analyze customer feedback with Net Promoter Score (NPS). \n
-      Learn more about the integration in the [Kustomer Help Center](https://help.kustomer.com/integrate-with-delighted-rku8RKIWt).
-    `,
+    iconUrl: 'https://cdn.glitch.global/623038c7-b614-42ff-ba4d-9cfe2753f237/icon.png?v=1654869205164',
+    description: `Automatically retrieve survey results from Delighted and display NPS data in the timeline. \n
+Use the Delighted app integration to allow agents to see a customer's NPS survey results from within the Kustomer timeline. The Delighted integration automatically creates and updates customer records whenever your customers submit an NPS survey through Delighted. This allows your team to have access to critical voice-of-customer data that can help set the tone for support interactions.
+**Delighted** is a customer feedback platform that provides a seamless way to collect and analyze customer feedback with Net Promoter Score (NPS). \n
+Learn more about the integration in the [Kustomer Help Center](https://help.kustomer.com/integrate-with-delighted-rku8RKIWt).`,
     clientId: '7b00e61887ea157d25512967d761a802',
     clientSecret: '7615e0cf6c0fd6df16f216057dc2bd56',
     roles: [
-      'org.admin',
       'org.user.customer.read',
-      'org.permission.customer.read'
+      'org.user.customer.write',
+      'org.user.kobject.write',
+      'org.permission.customer.read',
+      'org.permission.customer.create',
+      'org.permission.customer.update',
+      'org.permission.kobject.create',
+      'org.permission.kobject.kobject_*.create'
     ],
     url: 'https://delighted.glitch.me',
     env: 'qa',
@@ -43,7 +46,7 @@ const pkg = require('./package.json');
     default: false
   });
 
-  app.addKlass('net-promoter-score', {
+  app.withKlass('net-promoter-score', {
     icon: 'scoreboard',
     color: '#FF0000',
     metadata: {
@@ -56,11 +59,44 @@ const pkg = require('./package.json');
     }
   });
 
-  app.onHook('nps', async (orgId, data) => {
-    if (!data.event_data?.person?.email) return;
+  // app.withCustomView('timeline', '/views/timeline', {
+  //   context: 'expanded-timeline',
+  //   resource: 'kobject',
+  //   klass: 'net-promoter-score'
+  // });
 
-    const customer = await app.in(orgId).getCustomerByEmail(data.event_data.person.email);
-    app.log.info(customer);
+  app.onHook('nps', async (orgId, data) => {
+    let customer = await app.in(orgId).customers.getByEmail(data.event_data.person.email);
+
+    if (!customer) {
+      customer = await app.in(orgId).customers.create({
+        name: data.event_data.person.name,
+        emails: [{
+          email: data.event_data.person.email,
+          type: 'home',
+          verified: true
+        }],
+        custom: {
+          latestNpsNum: data.event_data.score
+        }
+      });
+    } else {
+      customer = await app.in(orgId).customers.update(customer.id, {
+        custom: {
+          latestNpsNum: data.event_data.score
+        }
+      });
+    }
+
+    await app.in(orgId).customers.createKObject(customer.id, 'net-promoter-score', {
+      title: `Delighted Rating ${data.event_data.score}`,
+      description: data.event_data.comment,
+      custom: {
+        updatedAt: new Date(data.event_data.updated_at),
+        scoreNum: data.event_data.score,
+        commentTxt: data.event_data.comment
+      }
+    });
   });
 
   try {
@@ -69,8 +105,8 @@ const pkg = require('./package.json');
       process.env.NODE_ENV === 'local'
     );
 
-    const token = await app.in('aacebo').getToken();
-    app.log.info(token);
+    // const token = await app.in('aacebo').getToken();
+    // app.log.info(token);
   } catch (err) {
     app.log.error(JSON.stringify(err, undefined, 2));
   }
